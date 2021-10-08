8c53dcdd9c979aceb51b15300646ad73b0946253
akhil14shukla <akhil14shukla@gmail.com>
2021-10-08 20:33:00 +0530

    Updated directory

---
+++ Codeforces/cf_747/A.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <math.h>
+#include <stack>
+#define pb push_back
+#define pop pop_back
+#define ll long long int
+#define vi vector<int>
+#define vl vector<long>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long>>
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep2(i, a, b) for (ll i = a; i <= b; i++)
+#define repr(i, a, b) for (ll i = a; i >= b; i--)
+#define I long_max
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define vpair vector<pair<int, int>>
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fir first
+#define sec second
+#define d (int)1e9 + 7
+#define INF (int)2e9 + 1
+#define el "\n"
+#define fs fastscan
+
+#define vrep(vec) for (const auto &value : vec)
+#define arep(arrat) for (const auto &value : array)
+using namespace std;
+
+bool sortinrev(const pair<int, int> &a,
+               const pair<int, int> &b)
+{
+    return (a.first > b.first);
+}
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+ll solve(){
+    
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //use auto instead of data types
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
